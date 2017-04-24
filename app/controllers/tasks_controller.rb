83160f084c441e61160ba97dc1958b7ce0c60b54
makoto.yoshikawa <maf11031001@gmail.com>
2017-04-24 04:19:42 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -25,11 +25,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = 'タスクが更新されました'
@@ -38,10 +36,10 @@ class TasksController < ApplicationController
       flash.now[:danger] = 'タスクは更新されませんでした'
       render :edit
     end
+    
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'タスクが削除されました'
@@ -49,8 +47,15 @@ class TasksController < ApplicationController
   end
   
   private
+   
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
+
+
+
